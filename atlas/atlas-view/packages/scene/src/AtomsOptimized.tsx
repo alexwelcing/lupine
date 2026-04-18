f036932502ba6719320653327ede92449120fa0f
Alex Welcing <alexwelcing@gmail.com>
2026-04-18 15:56:11 -0700

    fix(viewer): fix AtomsOptimized count prop override, add strict HTML fetch rejection to parser

--- atlas/atlas-view/packages/scene/src/AtomsOptimized.tsx
+++ atlas/atlas-view/packages/scene/src/AtomsOptimized.tsx
@@ -265,7 +265,7 @@ export function AtomsOptimized({
       ref={meshRef}
       args={[geometry, material, maxAtoms]}
       frustumCulled={false}
-      count={0} // Start empty, updated in effect
+      count={frame.natoms} // Correctly sync count via React Three Fiber
     >
       <instancedBufferAttribute attach="instanceColor" args={[colorArray, 3]} />
     </instancedMesh>
